8383fcf1e8d6ca8f53b3d15f240df04044b1a188
nikitalimbkar <nikitalimbkar@gmail.com>
2026-08-03 19:20:25 +0530

    Time: 120 ms (13.43%) | Memory: 51.3 MB (37.92%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -4,6 +4,9 @@ class Solution {
         Arrays.fill(isPrime,true);
 
         if(n <= 2) return 0;
+        isPrime[0] = false;
+        isPrime[1] = false;
+        
         int count = 0;
         for(int i=2; i<n; i++){
             if(isPrime[i]){
